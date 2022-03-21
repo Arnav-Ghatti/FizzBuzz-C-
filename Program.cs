d17e2b35b19672b8224028c5982ffc205ada2c74
Arnav <arnavghatti.cb@gmail.com>
2022-03-21 16:21:51 +0530

    Finished Project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// All numbers from 1 to 100
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 != 0) // Checking if number is divisible by 3 and not by 5
+    {
+        Console.WriteLine("Fizz");
+    } else if (i % 5 == 0 && i % 3 != 0) // Checking if number is divisible by 5 and not by 3
+    {
+        Console.WriteLine("Buzz");
+    } else if (i % 15 == 0) // Checking if number is divisible by 15 (Both by 3 and 5)
+    {
+        Console.WriteLine("FizzBuzz");
+    } else
+    {
+        Console.WriteLine(i);
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Hey everyone!
+
+I just started learning C# and this is the first project I created.
+If you are a programmer, then I am sure you know about FizzBuzz.
+
+## What is FizzBuzz?
+FizzBuzz is a task where the programmer is asked to print numbers from 1 to 100, but here’s the catch, multiple of three should print “Fizz” and similarly print “Buzz” for multiples of 5 and lastly print “FizzBuzz” for multiples of three and five.
+
+WRITTEN IN C#
